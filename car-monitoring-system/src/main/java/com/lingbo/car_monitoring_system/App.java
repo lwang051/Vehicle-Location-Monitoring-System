784049ad3c941feb52a13fc0b40be8adfd7cf60f
Lingbo Wang <lwang051@fiu.edu>
2018-06-12 14:36:22 -0400

    Car Monitoring System

---
+++ car-monitoring-system/src/main/java/com/lingbo/car_monitoring_system/App.java
@@ -0,0 +1,11 @@
+package com.lingbo.car_monitoring_system;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Car Monitoring System!" );
+        System.out.println("commits from car monitoring system branch");
+    }
+}
