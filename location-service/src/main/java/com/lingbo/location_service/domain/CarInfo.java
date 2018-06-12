4774bc4e0df8afadf27129142dfc81aa40e9f228
Lingbo Wang <lwang051@fiu.edu>
2018-06-12 18:16:49 -0400

    add car info

---
+++ location-service/src/main/java/com/lingbo/location_service/domain/CarInfo.java
@@ -0,0 +1,23 @@
+package com.lingbo.location_service.domain;
+
+import javax.persistence.Embeddable;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class CarInfo {
+	
+	private final String vin;
+	private String engineMake;
+	private String customerName;
+	private String number;
+	
+}
