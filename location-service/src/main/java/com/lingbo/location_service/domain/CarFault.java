7e404c03912e622f2e82e3cfbb25a68f6725ded3
Lingbo Wang <lwang051@fiu.edu>
2018-06-12 17:33:25 -0400

    add car fault

---
+++ location-service/src/main/java/com/lingbo/location_service/domain/CarFault.java
@@ -0,0 +1,18 @@
+package com.lingbo.location_service.domain;
+
+import javax.persistence.Embeddable;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@Embeddable
+@RequiredArgsConstructor
+public class CarFault {
+	
+	private final String vin;
+	private Long spn;
+	private Long fmi;
+	
+}
