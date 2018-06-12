d9ec17b8cf2c279a69447ab137b068a39e2a5476
Lingbo Wang <lwang051@fiu.edu>
2018-06-12 16:45:11 -0400

    add LocationServiceApplication

---
+++ location-service/src/main/java/com/lingbo/location_service/LocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lingbo.location_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationServiceApplication {
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(LocationServiceApplication.class, args);
+    }
+    
+}
