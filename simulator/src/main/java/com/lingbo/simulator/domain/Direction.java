02996941d63ade4c2cde23b203f433ddb8f8dc87
Lingbo Wang <lwang051@fiu.edu>
2018-06-16 18:19:09 -0400

    Added Direction

---
+++ simulator/src/main/java/com/lingbo/simulator/domain/Direction.java
@@ -0,0 +1,16 @@
+package com.lingbo.simulator.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Direction {
+	
+	private String from;
+	private String to;
+	
+}
