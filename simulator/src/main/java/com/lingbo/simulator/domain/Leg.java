d16572a68e7031e6bc48b7430a725325d4cbd02c
Lingbo Wang <lwang051@fiu.edu>
2018-06-16 18:31:01 -0400

    Added Leg

---
+++ simulator/src/main/java/com/lingbo/simulator/domain/Leg.java
@@ -0,0 +1,17 @@
+package com.lingbo.simulator.domain;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Leg {
+	
+	private Integer id;
+    private Point start;
+    private Point end;
+    private Double length;
+    private Double heading;
+	
+}
