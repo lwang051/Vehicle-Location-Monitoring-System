ab5f6c9501694d4a85ea224805faf7843d6ad5aa
Lingbo Wang <lwang051@fiu.edu>
2018-06-17 20:07:55 -0400

    Added more files

---
+++ messaging-service/src/main/java/com/lingbo/messaging_service/FleetLocationIngestApplication.java
@@ -0,0 +1,15 @@
+package com.lingbo.messaging_service;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class FleetLocationIngestApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FleetLocationIngestApplication.class);
+    }
+
+}
---
+++ messaging-service/src/main/java/com/lingbo/messaging_service/rest/VehiclePositionsSource.java
@@ -0,0 +1,27 @@
+package com.lingbo.messaging_service.rest;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableBinding(Source.class)
+@RestController
+public class VehiclePositionsSource {
+
+    @Autowired
+    private MessageChannel output;
+
+    @RequestMapping(path = "/api/locations", method = RequestMethod.POST)
+    public void locations(@RequestBody String positionInfo) {
+        this.output.send(MessageBuilder.withPayload(positionInfo).build());
+    }
+
+}
