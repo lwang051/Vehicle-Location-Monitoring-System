0f18980bf0650f59725c24f3bf8f7e7915bc957e
Lingbo Wang <lwang051@fiu.edu>
2018-06-18 23:19:59 -0400

    Moved some dependencies to the systems that really need them. Fixed the runtime bean factory error

---
+++ car-monitoring-system/src/main/java/com/lingbo/car_monitoring_system/CarMonitoringSystemApplication.java
@@ -0,0 +1,15 @@
+package com.lingbo.car_monitoring_system;
+
+/*
+* This is an aggregation system that consolidates all the modules.
+* All that matters is the pom.xml.
+* No more attention is needed here.
+*/
+
+public class CarMonitoringSystemApplication {
+	
+    public static void main(String[] args) {
+        
+    }
+    
+}
