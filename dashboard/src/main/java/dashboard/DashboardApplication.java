13e7d0701d8ea7454cf0550e270038f9e7967583
Lingbo Wang <lwang051@fiu.edu>
2018-06-25 23:16:42 -0400

    Registered other services in eureka

--- dashboard/src/main/java/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/dashboard/DashboardApplication.java
@@ -18,10 +18,12 @@ package dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class DashboardApplication {
 
     public static void main(String[] args) {
---
+++ sh/eureka-service-start.sh
@@ -0,0 +1,1 @@
+java -jar ../eureka-service/target/eureka-service-1.0.0.BUILD-SNAPSHOT.jar
