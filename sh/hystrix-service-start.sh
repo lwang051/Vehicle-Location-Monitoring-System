387b86751d04c291d78926b79b8453749881ab2d
Lingbo Wang <lwang051@fiu.edu>
2018-06-26 00:16:43 -0400

    Added hystrix-service

---
+++ sh/hystrix-service-start.sh
@@ -0,0 +1,1 @@
+java -jar ../hystrix-service/target/hystrix-service-1.0.0.BUILD-SNAPSHOT.jar
--- simulation-service/src/main/java/com/lingbo/simulation_service/GpsSimulatorApplication.java
+++ simulation-service/src/main/java/com/lingbo/simulation_service/GpsSimulatorApplication.java
@@ -3,6 +3,7 @@ package com.lingbo.simulation_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class GpsSimulatorApplication {
 
     public static void main(String[] args) throws Exception {
